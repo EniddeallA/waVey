ef45989a7f18c659a93bb0dddbf4003c625ece3a
eniddealla <allaeddinekhalid@gmail.com>
2020-01-19 01:07:34 +0100

    switching to OOP

--- vibe.py
+++ vibe.py
@@ -3,19 +3,29 @@ from gtts import gTTS
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from glob import glob
 import librosa as lr
 
-tts = gTTS('test', lang='en')
-tts.save('./saved/hello.mp3')
-
-data_dir = './saved'
-audio_files = glob(data_dir + '/*.mp3')
-
-audio, sfreq = lr.load(audio_files[0])
-time = np.arange(0, len(audio)) / sfreq
-
-fig, ax = plt.subplots()
-ax.plot(time, audio)
-ax.set(xlabel='Time (s)', ylabel='Sound Amplitude')
-plt.show()
+class textToWave:
+    def __init__(self, text, lang) :
+        self.text = text
+        self.lang = lang
+        self.inputPath = './sounds/'
+        self.outputPath = './plots/'        
+        self.audio = self.inputPath + self.text +'.mp3'
+        self.wave = self.outputPath + self.text +'.png'
+    
+    def textToAudioToWave(self):
+        tts = gTTS(self.text, self.lang)
+        tts.save(self.audio)
+        audio_files = glob(self.audio)
+        audio, sfreq = lr.load(audio_files[0])
+        time = np.arange(0, len(audio)) / sfreq
+        fig, ax = plt.subplots()
+        ax.plot(time, audio)
+        plt.axis('off')
+        plt.savefig(self.wave)
+        
+tw = textToWave('hello world', 'en')
+tw.textToAudioToWave()
