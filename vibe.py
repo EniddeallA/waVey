4eaf2710f78f997e2aa7cc88013b043e9959473f
eniddealla <allaeddinekhalid@gmail.com>
2020-01-20 06:10:36 +0100

    U can configure the aestethic of the sound waves

--- vibe.py
+++ vibe.py
@@ -6,15 +6,18 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from glob import glob
 import librosa as lr
+import sys
 
 class textToWave:
-    def __init__(self, text, lang) :
+    def __init__(self, text, lang, WaveColor, BgColor) :
         self.text = text
         self.lang = lang
         self.inputPath = './sounds/'
         self.outputPath = './plots/'        
         self.audio = self.inputPath + self.text +'.mp3'
-        self.wave = self.outputPath + self.text +'.png'
+        self.WaveColor = WaveColor
+        self.BgColor = BgColor
+        self.Outputwave = self.outputPath + self.text + '_' + self.WaveColor + '_' + self.BgColor + '.png'
     
     def textToAudioToWave(self):
         tts = gTTS(self.text, lang=self.lang)
@@ -23,9 +26,16 @@ class textToWave:
         audio, sfreq = lr.load(audio_files[0])
         time = np.arange(0, len(audio)) / sfreq
         fig, ax = plt.subplots()
-        ax.plot(time, audio)
+        ax.plot(time, audio, color=self.WaveColor)
         plt.axis('off')
-        plt.savefig(self.wave)
-        
-tw = textToWave('hello world', 'en')
+        plt.savefig(self.Outputwave, facecolor=self.BgColor)
+
+############# EDIT HERE ##############
+text = 'Hello World!'
+lang = 'en'
+WaveColor = 'white'
+BgColor = 'black'
+######################################
+
+tw = textToWave(text, lang, WaveColor, BgColor)
 tw.textToAudioToWave()
